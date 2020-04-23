dd5ef9b3592ecc409dce4f0d895a3381f24609f0
YishaiBerg <ymb123@gmail.com>
2020-04-24 02:11:59 +0300

    remove demo files

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
-// import DynamicMarquee from 'vue-dynamic-marquee';
-import DynamicMarquee from './dydemo/DynamicMarquee';
+import DynamicMarquee from 'vue-dynamic-marquee';
 import 'prismjs';
 // import 'prismjs/themes/prism.css';
 import 'prismjs/themes/prism-tomorrow.css';
